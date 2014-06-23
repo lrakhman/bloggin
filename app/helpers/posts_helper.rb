1a4a42a06cd0be43c0ea6e371e47c65ecebce0ac
lrakhman <lana.rakhman@gmail.com>
2014-06-22 22:45:44 -0500

    holy shit pics work now too

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
 	def post_params
-  	params.require(:post).permit(:title, :body)
+  	params.require(:post).permit(:title, :body, :tag_list, :image)
 	end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 end
